ddac123880a2c485d7c82b90ea9843cd2f173b3b
Your Name <you@example.com>
2017-02-11 16:16:11 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name text,
+  age integer,
+  gender char(1), /*(the choices would be "M" or "F"),*/
+  color text,
+  temperament text,
+  alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 2, "M", "blue", "angry", 1),
+(2,"Rowdy",3 ,"F", "brown", "moody", 1),
+(3,"Tabitha",7,"F","beige","happy",0),
+(4,"Sergeant Brown",4,"M","brown","calm",1),
+(5,"Melissa",6,"F","black","calm",1),
+(6,"Grinch",5,"M","brown","moody",1),
+(7,"Wendy",12,"F","black","happy",1),
+(8,Null,8,"M","brown","viscious",1);
